c51117fc31fceedec8be775b1f7ff162eecc59bb
Jack Rifkin <jack.rifkin@icloud.com>
2023-11-26 19:50:59 -0500

    added home page + refactored routing

--- src/Mixr/index.js
+++ src/Mixr/index.js
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Profile from "./Profile";
+import Home from "./Home";
 
 function Mixr() {
     // Put Your Screens in Routes, will go under nav bar
@@ -8,7 +9,10 @@ function Mixr() {
         <div>
             <Navigation />
             <Routes>
-                <Route path="Profile/:userID" element={<Profile/>}/>
+                <Route path="/" element={<Navigate to="/Home"/>} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Home/:userID" element={<Home />} />
+                <Route path="/Profile/:userID" element={<Profile/>}/>
             </Routes>
         </div>
     )
