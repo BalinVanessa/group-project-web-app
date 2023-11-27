5a3d14f5850885363578b2a3b45ac3191d635c46
nandini-ghosh <nandini.ghosh2094@gmail.com>
2023-11-26 20:19:33 -0500

    log in/sign up

--- src/Mixr/index.js
+++ src/Mixr/index.js
@@ -1,6 +1,8 @@
 import Navigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Profile from "./Profile";
+import Login from "./Login";
+import SignUp from "./Login/SignUp";
 
 function Mixr() {
     // Put Your Screens in Routes, will go under nav bar
@@ -9,6 +11,8 @@ function Mixr() {
             <Navigation />
             <Routes>
                 <Route path="Profile/:userID" element={<Profile/>}/>
+                <Route path="Login" element={<Login/>}/>
+                <Route path="SignUp" element={<SignUp/>}/>
             </Routes>
         </div>
     )
