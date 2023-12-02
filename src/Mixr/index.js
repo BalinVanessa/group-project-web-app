110235e02175578d5b377de0353fe4f19314cd67
Jack Rifkin <jack.rifkin@icloud.com>
2023-12-01 19:04:42 -0500

    integrate users a6

--- src/Mixr/index.js
+++ src/Mixr/index.js
@@ -4,6 +4,9 @@ import Profile from "./Profile";
 import Login from "./Login";
 import SignUp from "./Login/SignUp";
 import Home from "./Home";
+import Signin from "../users/signin";
+import Account from "../users/account";
+import UserTable from "../users/table";
 
 function Mixr() {
     // Put Your Screens in Routes, will go under nav bar
@@ -11,13 +14,17 @@ function Mixr() {
         <div>
             <Navigation />
             <Routes>
-                <Route path="Profile/:userID" element={<Profile/>}/>
-                <Route path="Login" element={<Login/>}/>
-                <Route path="SignUp" element={<SignUp/>}/>
-                <Route path="/" element={<Navigate to="/Home"/>} />
+                <Route path="Profile/:userID" element={<Profile />} />
+                <Route path="Login" element={<Login />} />
+                <Route path="SignUp" element={<SignUp />} />
+                <Route path="/" element={<Navigate to="/Home" />} />
                 <Route path="/Home" element={<Home />} />
                 <Route path="/Home/:userID" element={<Home />} />
-                <Route path="/Profile/:userID" element={<Profile/>}/>
+                <Route path="/Profile/:userID" element={<Profile />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/account/:id" element={<Account />} />
+                <Route path="/admin/users" element={<UserTable />} />
             </Routes>
         </div>
     )
