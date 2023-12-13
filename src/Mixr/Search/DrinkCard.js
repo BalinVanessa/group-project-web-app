853c17924c24ecce2a9d012d5899e2157ff278d7
Jack Rifkin <jack.rifkin@icloud.com>
2023-12-13 18:12:50 -0500

    added cocktail searching

---
+++ src/Mixr/Search/DrinkCard.js
@@ -0,0 +1,30 @@
+import { FaStar } from "react-icons/fa6";
+
+function DrinkCard({ drink }) {
+    function makeStars(num) {
+        const arr = [];
+        for (let i = 0; i < num; i++) {
+            arr.push(
+                <FaStar />
+            );
+        }
+        return arr;
+    }
+
+    return (
+        <div className="drinkCard mxr-med-blue-bg">
+            <div className="align-top">
+                <img className="drinkCard-img" src={drink.strDrinkThumb || "./Images/EspressoMartini.jpg"} />
+            </div>
+            <div className="drinkCard-text">
+                <h4>{drink.strDrink}</h4>
+                <div className="d-inline">
+                    {makeStars(4)}
+                </div>
+                <p className="ellipsis-wrap">{drink.strInstructions}</p>
+            </div>
+        </div>
+    );
+}
+
+export default DrinkCard;
--- src/Mixr/Util/ResponsiveCenterDiv.js
+++ src/Mixr/Util/ResponsiveCenterDiv.js
@@ -1,11 +1,11 @@
 function ResponsiveCenterDiv({children, className}) {
     return (
         <div className={`row ${className ? className : ''}`}>
-            <div className="col-3 d-none d-lg-block" />
+            <div className="col-2 d-none d-lg-block" />
             <div className="col d-flex justify-content-between align-items-center">
                 {children}
             </div>
-            <div className="col-3 d-none d-lg-block" />
+            <div className="col-2 d-none d-lg-block" />
         </div>
     );
 }
